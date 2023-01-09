5b54039915fad658664a29ed0892effcaadc0723
Aliez22 <820460891@qq.com>
2023-01-09 18:15:12 +0800

    add ch.1 leetcode27

---
+++ 1_Array/lc27.remove-element.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/*
+* @method: 快慢指针，一次遍历。快慢一起移动，快值赋慢值，慢满足条件停下
+* @evaluation: O(n)
+*/
+
+class Solution27 {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int fast = 0, slow = 0;
+        for(;fast < nums.size(); fast++){
+            if(nums[fast] != val){
+                nums[slow++] = nums[fast];
+            }
+        }
+        return slow;
+    }
+};
