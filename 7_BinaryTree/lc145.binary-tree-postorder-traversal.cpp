c9d40a29895a5a8b35295fb22454729fd132c72d
Aliez22 <820460891@qq.com>
2023-02-18 11:13:41 +0800

    update ch.7 leetcode145 diedai postorder traversal

--- 7_BinaryTree/lc145.binary-tree-postorder-traversal.cpp
+++ 7_BinaryTree/lc145.binary-tree-postorder-traversal.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <algorithm>
 #include "TreeNode.h"
 using namespace std;
 
 /*
-* @method: 递归法，二叉树后序遍历（左-右-中）
+* @method: 二叉树后序遍历（左-右-中）
 */
 
 class Solution145 {
@@ -18,6 +20,30 @@ public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> res;
         traversal(root, res);
+        return res;
+    }
+    // 迭代法，效仿先序遍历迭代法，调整孩子入栈顺序，得到中右左的结果，再反转结果数组
+    void reverse(vector<int>& vec){
+        int i = 0, j = vec.size() - 1;
+        while(i<j){
+            swap(vec[i++], vec[j--]);
+        }
+    }
+    vector<int> postorderTraversal1(TreeNode* root){
+        vector<int> res;
+        stack<TreeNode *> st;
+        if(root!=nullptr)
+            st.push(root);
+        while(!st.empty()){
+            TreeNode *node = st.top();
+            st.pop();
+            res.push_back(node->val);
+            if(node->left)
+                st.push(node->left);
+            if(node->right)
+                st.push(node->right);
+        }
+        reverse(res);
         return res;
     }
 };
