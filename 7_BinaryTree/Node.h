4fe29ed02bb2b826d4aeeb641c668dbc458c4330
Aliez22 <820460891@qq.com>
2023-02-19 16:26:19 +0800

    add ch.7 Node.h for N-tree

---
+++ 7_BinaryTree/Node.h
@@ -0,0 +1,23 @@
+#ifndef __NODE_H
+#define __NODE_H
+
+#include <vector>
+
+class Node {
+public:
+    int val;
+    std::vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, std::vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+#endif
