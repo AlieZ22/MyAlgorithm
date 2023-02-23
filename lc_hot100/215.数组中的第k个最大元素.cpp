84698bd6194a47ed98677790584da68d02bb5d4f
Aliez22 <820460891@qq.com>
2023-02-23 16:50:42 +0800

    add hot 215

--- lc_hot100/215.数组中的第k个最大元素.cpp
+++ lc_hot100/215.数组中的第k个最大元素.cpp
@@ -2,9 +2,40 @@
 #include <vector>
 using namespace std;
 
+
+/*
+* @method: 建立一个大根堆，做k-1次删除之后，堆顶元素就是答案
+*/
 class Solution_hot215 {
 public:
+    void adjustHeap(vector<int>& nums, int i, int heapSize){
+        int l = i * 2 + 1, r = i * 2 + 2, largest = i;
+        if(l < heapSize && nums[l] > nums[largest]){
+            largest = l;
+        }
+        if(r < heapSize && nums[r] > nums[largest]){
+            largest = r;
+        }
+        if(largest != i){
+            swap(nums[i], nums[largest]);
+            adjustHeap(nums, largest, heapSize);
+        }
+    }
+    void buildMaxHeap(vector<int>& nums, int heapSize){
+        // 每个非叶节点的根节点依此调整
+        for (int i = heapSize / 2; i >= 0; --i){
+            adjustHeap(nums, i, heapSize);
+        }
+    }
     int findKthLargest(vector<int>& nums, int k) {
-        
+        int heapSize = nums.size();
+        buildMaxHeap(nums, heapSize);
+        // 删除过程，每次删除堆顶，将最后一个元素移到堆顶，然后调整堆结构
+        for (int i = nums.size() - 1; i >= nums.size() - k + 1; --i){
+            swap(nums[0], nums[i]);
+            --heapSize;
+            adjustHeap(nums, 0, heapSize);
+        }
+        return nums[0];
     }
 };
