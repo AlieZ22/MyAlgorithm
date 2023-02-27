932505bfb1706b96b916e1c91916b8166b7fccb9
Aliez22 <820460891@qq.com>
2023-02-27 16:26:16 +0800

    add hot 46

---
+++ codeTop/46.全排列.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+* @method:回溯
+*/
+
+class Solution_hot46 {
+public:
+    vector<vector<int> > res;
+    vector<int> path;
+    void backtrack(vector<int>& choices){
+        if(choices.size()==0){
+            res.push_back(path);
+        }
+        for(int i=0;i<choices.size();++i){
+            vector<int> newChoices(choices);
+            newChoices.erase(newChoices.begin()+i);
+            path.push_back(choices[i]);
+            backtrack(newChoices);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        backtrack(nums);
+        return res;
+    }
+};
