d2e74dc19b46722c2b3358930b3870a9af5d4e2c
Aliez22 <820460891@qq.com>
2023-03-30 13:53:41 +0800

    update algorithm test

--- algTest.cpp
+++ algTest.cpp
@@ -1,60 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+string s;        // 十六进制数
+unordered_map<char, int> umap;
 
-class Solution {
-public:
-    PNN reverseSubList(ListNode* front, ListNode* tail){
-        ListNode *pre = tail->next;
-        ListNode *node = front;
-        while(pre!=tail){
-            ListNode *nex = node->next;
-            node->next = pre;
-            pre = node;
-            node = nex;
-        }
-        return PNN{tail, front};
+void initMap(){
+    for(int i=0; i<=9; ++i){
+        umap[char(i+'0')] = i;
     }
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode *dummy = new ListNode(-1,head);
-        ListNode *pre = dummy;
-        ListNode *nex = dummy;
-        while(nex!=nullptr){
-            int _k = k;
-            while(_k--){
-                nex = nex->next;
-                if(nex==nullptr){
-                    return dummy->next;
-                }
-            }
-            ListNode *front = pre->next;
-            PNN nodes = reverseSubList(front, nex);
-            pre->next = nodes.first;
-            pre = nodes.second;
-            nex = nodes.second;
-        }
-        return dummy->next;
-    }
-};
+    umap['A'] = 10; umap['a'] = 10;
+    umap['B'] = 11; umap['b'] = 11;
+    umap['C'] = 12; umap['c'] = 12;
+    umap['D'] = 13; umap['d'] = 13;
+    umap['E'] = 14; umap['e'] = 14;
+    umap['F'] = 15; umap['f'] = 15;
+}
 
+// 进制转换，将16进制的数转化为10进制
 int main(){
-    Solution s;
-    ListNode *n5 = new ListNode(5,nullptr);
-    ListNode *n4 = new ListNode(4,n5);
-    ListNode *n3 = new ListNode(3,n4);
-    ListNode *n2 = new ListNode(2,n3);
-    ListNode *n1 = new ListNode(1,n2);
-    ListNode *res = s.reverseKGroup(n1, 2);
-    while(res!=nullptr){
-        cout<<res->val<<endl;
-        res = res->next;
+    cin>>s;
+    initMap();
+    int res = 0;
+    int j = 0;
+    for(int i=s.size()-1; i>=2; --i){
+        res += umap[s[i]]*pow(16,j++);
     }
+    cout<<res<<endl;
     return 0;
 }
