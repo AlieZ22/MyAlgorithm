d76dc635fccea7cba3778afa9a4ec150b918c701
Aliez22 <820460891@qq.com>
2023-04-21 19:23:42 +0800

    udpate algTest

--- algTest.cpp
+++ algTest.cpp
@@ -1,30 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string s;        // 十六进制数
-unordered_map<char, int> umap;
+const int M = 7;
 
-void initMap(){
-    for(int i=0; i<=9; ++i){
-        umap[char(i+'0')] = i;
-    }
-    umap['A'] = 10; umap['a'] = 10;
-    umap['B'] = 11; umap['b'] = 11;
-    umap['C'] = 12; umap['c'] = 12;
-    umap['D'] = 13; umap['d'] = 13;
-    umap['E'] = 14; umap['e'] = 14;
-    umap['F'] = 15; umap['f'] = 15;
-}
-
-// 进制转换，将16进制的数转化为10进制
+// 尝试用delete释放malloc分配的内存
 int main(){
-    cin>>s;
-    initMap();
-    int res = 0;
-    int j = 0;
-    for(int i=s.size()-1; i>=2; --i){
-        res += umap[s[i]]*pow(16,j++);
+    int *nums;
+    nums = (int*)malloc(sizeof(int)*M);
+    for(int i=0;i<M;++i){
+        nums[i]=i;
     }
-    cout<<res<<endl;
+    delete [] nums;
+    cout<< "end" << endl;
     return 0;
 }
