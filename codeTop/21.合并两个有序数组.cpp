efefd6fba5067b4cf6c07aa6998fed0592145509
Aliez22 <820460891@qq.com>
2023-08-12 16:36:34 +0800

    优化代码结构

--- codeTop/21.合并两个有序数组.cpp
+++ codeTop/21.合并两个有序数组.cpp
@@ -4,34 +4,28 @@ using namespace std;
 
 class Solution_hot21 {
 public:
-    // 在pre后面添加node
-    void addNode(ListNode *pre, ListNode *node){
-        node->next = pre->next;
-        pre->next = node;
-        // 注意，这里不能改变pre，参数pre与外侧pre不同，只是一个拷贝
-    }
-    // 为list1添加头节点，每次连在list1上
+    // 简单一点考虑，直接类似归并排序
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode *dummy = new ListNode(0, list1);
+        ListNode *dummy = new ListNode(-1, nullptr);
         ListNode *pre = dummy;
-        ListNode *a = list1, *b = list2;
-        while(a!=nullptr && b!=nullptr){
-            if(b->val <= a->val){
-                ListNode *node = b;
-                b = b->next;
-                addNode(pre, node);
-                pre = pre->next;
+        while(list1!=nullptr && list2!=nullptr){
+            if(list1->val <= list2->val){
+                pre->next = list1;
+                list1 = list1->next;
             }else{
-                pre = a;
-                a = a->next;
+                pre->next = list2;
+                list2 = list2->next;
             }
+            pre = pre->next;
         }
-        // 把b剩余的加到a, a已经空了
-        while(b!=nullptr){
-            ListNode *node = b;
-            b = b->next;
-            node->next = nullptr;
-            pre->next = node;
+        while(list1 != nullptr){
+            pre->next = list1;
+            list1 = list1->next;
+            pre = pre->next;
+        }
+        while(list2 != nullptr){
+            pre->next = list2;
+            list2 = list2->next;
             pre = pre->next;
         }
         ListNode *res = dummy->next;
