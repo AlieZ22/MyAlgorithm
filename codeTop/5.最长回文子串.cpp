81513a2c7ed9325d6fb176420a4e05d1f418d002
Aliez22 <820460891@qq.com>
2023-08-14 22:10:07 +0800

    update 最长回文子串，去掉len变量

--- codeTop/5.最长回文子串.cpp
+++ codeTop/5.最长回文子串.cpp
@@ -10,19 +10,11 @@ public:
         int n = s.size();
         for(int k=0;k<n;++k){     // 遍历每一个字符，从中间向两边扩散
             int i=k-1, j=k+1;
-            int len =1;
             // 先同样字符扩散
-            while(i>=0 && s[i] == s[k]){
-                ++len;
-                --i;
-            }
-            while(j<n && s[j] == s[k]){
-                ++len;
-                ++j;
-            }
+            while(i>=0 && s[i] == s[k]){ --i; }
+            while(j<n && s[j] == s[k]){ ++j; }
             // 再不同字符扩散
             while(i>=0 && j<n && s[i] == s[j]){
-                len+=2;
                 ++j;
                 --i;
             }
