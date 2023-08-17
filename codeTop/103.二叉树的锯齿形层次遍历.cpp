a61258fd83d6d09526de7f362ce620bb7dd34a2e
Aliez22 <820460891@qq.com>
2023-08-17 22:00:28 +0800

    update 二叉树锯齿层次遍历

--- codeTop/103.二叉树的锯齿形层次遍历.cpp
+++ codeTop/103.二叉树的锯齿形层次遍历.cpp
@@ -11,31 +11,29 @@ using namespace std;
 class Solution_hot103 {
 public:
     template<typename T>
-    void reverseVec(vector<T>& vec){
+    void reverse(vector<T>& vec){
         int i=0, j=vec.size()-1;
         while(i<j){
-            swap(vec[i++],vec[j--]);
+            swap(vec[i++], vec[j--]);
         }
     }
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
         queue<TreeNode*> qt;
-        vector<vector<int> > res;
         if(root != nullptr) qt.push(root);
-        bool dirSign = 0;       // 0：直接放，1：反转tmp数组
+        int k=0;
         while(!qt.empty()){
             int size = qt.size();
             vector<int> tmp;
             for(int i=0; i<size; ++i){
-                TreeNode *node = qt.front();
+                TreeNode* node = qt.front();
                 qt.pop();
                 tmp.push_back(node->val);
                 if(node->left) qt.push(node->left);
                 if(node->right) qt.push(node->right);
             }
-            if(dirSign){
-                reverseVec(tmp);
-            }
-            dirSign = !dirSign;
+            if(k%2 == 1){ reverse(tmp); }
+            ++k;
             res.push_back(tmp);
         }
         return res;
