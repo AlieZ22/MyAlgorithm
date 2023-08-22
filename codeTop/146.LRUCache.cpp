bfe7b8d3ed2025ca3132876f626c1d32631d3292
Aliez22 <820460891@qq.com>
2023-08-22 20:52:10 +0800

    update LRUCache带过期时间

--- codeTop/146.LRUCache.cpp
+++ codeTop/146.LRUCache.cpp
@@ -1,13 +1,18 @@
 #include <bits/stdc++.h>
 #include <mutex>
+#include <ctime>
 using namespace std;
+
+const int TTL = 5;         // 过期时间间隔，5s
 
 struct DLinkNode{
     int key, value;
+    time_t expire_time;    // 带过期时间
     DLinkNode *prev;
     DLinkNode *next;
     DLinkNode():key(0), value(0), prev(nullptr), next(nullptr){}
     DLinkNode(int k, int v): key(k), value(v), prev(nullptr), next(nullptr){}
+    DLinkNode(int k, int v, time_t et):key(k),value(v),expire_time(et),prev(nullptr),next(nullptr){}
 };
 
 class LRUCache {
@@ -33,6 +38,11 @@ public:
             return -1;
         }
         DLinkNode *node = cache[key];
+        // 访问时判断该元素是否过期
+        if(difftime(node->expire_time, time(nullptr)) <= 0){   // 返回-1
+            removeNode(node);
+            return -1;
+        }
         moveToHead(node);
         return node->value;
     }
@@ -42,11 +52,25 @@ public:
         if(!cache.count(key)){
             // 创建新的节点
             DLinkNode *node = new DLinkNode(key, value);
+            // 创建带过期时间的节点
+            DLinkNode *node = new DLinkNode(key, value, time(nullptr)+TTL);   // time(nullptr)表示当前时间
             cache[key] = node;
             addToHead(node);
             ++size;
-            if(size > capacity){         // 超出容量，删除尾部节点
-                DLinkNode *node = tail->prev;
+            if(size > capacity){         // 超出容量，删除过期->尾部节点
+                DLinkNode *node = nullptr;
+                // 先试图找一个过期节点
+                time_t currTime = time(nullptr);
+                for(auto it=cache.begin(); it!=cache.end(); ++it){
+                    if(difftime(it->second->expire_time, currTime) <= 0){
+                        node = it->second;
+                        break;
+                    }
+                }
+                // 没找到过期节点，就删除尾部节点
+                if(node == nullptr){
+                    node = tail->prev;
+                }
                 removeNode(node);
                 cache.erase(node->key);
                 delete node;
