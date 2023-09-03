204ce28e95de540ffd7fbc5e55949a1675ca5ee6
Aliez22 <820460891@qq.com>
2023-09-03 23:13:02 +0800

    update 用栈模拟队列

--- 6_StackAndQueue/lc232.用栈实现队列.cpp
+++ 6_StackAndQueue/lc232.用栈实现队列.cpp
@@ -18,29 +18,30 @@ public:
     }
     
     int pop() {
-        int x = this->peek();
+        if(s2.size() == 0){
+            pour();
+        }
+        int res = s2.top();
         s2.pop();
-        return x;
+        return res;
     }
     
     int peek() {
-        // 是否需要移动元素
-        moveElement();
-        // 取s2的top
-        return s2.empty()?-1:s2.top();
+        if(s2.size() == 0){
+            pour();
+        }
+        return s2.top();
     }
     
     bool empty() {
-        return s1.size() + s2.size() > 0? false: true;
+        return s1.size() + s2.size() == 0? true: false;
     }
 
-    void moveElement(){
-        // 注意：只有当s2空的时候，转移s1的元素至s2，让s2的top为队列头
-        if(s2.empty()){
-            while(!s1.empty()){
+    // 将s1中元素倾倒至s2
+    void pour(){
+        while(!s1.empty()){
             s2.push(s1.top());
             s1.pop();
-            }
         }
     }
 };
