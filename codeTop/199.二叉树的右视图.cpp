1982709680c21876da7fe8ee10e74ae2afbace43
Aliez22 <820460891@qq.com>
2023-09-04 13:50:42 +0800

    add 二叉树的右视图

---
+++ codeTop/199.二叉树的右视图.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include "TreeNode.h"
+using namespace std;
+
+class Solution_hot199 {
+public:
+    // 层次遍历，每次取每层最右边的值
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        TreeNode *node = root;
+        queue<TreeNode*> qt;
+        if(node != nullptr) qt.push(node);
+        while(!qt.empty()){
+            int size = qt.size();
+            vector<int> tmp(size);
+            for(int i=0; i<size; ++i){
+                node = qt.front();
+                qt.pop();
+                tmp[i] = node->val;
+                if(node->left) qt.push(node->left);
+                if(node->right) qt.push(node->right);
+            }
+            res.emplace_back(tmp.back());
+        }
+        return res;
+    }
+};
