2ff84248ad1c529c2f9811b6870be46eab955390
Aliez22 <820460891@qq.com>
2023-10-04 19:43:59 +0800

    update 2 两数之和

--- codeTop/2.两数相加.cpp
+++ codeTop/2.两数相加.cpp
@@ -4,40 +4,40 @@ using namespace std;
 
 class Solution_hot2 {
 public:
-    int getLen(ListNode *list){
-        int cnt = 0;
-        while(list != nullptr){
-            ++cnt;
-            list = list->next;
-        }
-        return cnt;
-    }
+    // 直接用归并的方式做
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int len1 = getLen(l1), len2 = getLen(l2);
-        if(len2 > len1) return addTwoNumbers(l2,l1);
-        int up = 0, x=0;          // 进位
-        ListNode *head = l1, *pre=nullptr;
-        // 修改建立在l1上, l1总是最长的
+        ListNode dummy;
+        ListNode *pre = &dummy;
+        int x, up=0;
         while(l1 != nullptr && l2 != nullptr){
-            x = (l1->val + l2->val + up) % 10;
-            up = (l1->val + l2->val + up) / 10;       // 表示进位
-            l1->val = x;
-            pre = l1;
+            x = (up + l1->val + l2->val) % 10;
+            up = (up + l1->val + l2->val) / 10;
+            ListNode *node = new ListNode(x);
+            pre->next = node;
+            pre = pre->next;
             l1 = l1->next;
             l2 = l2->next;
         }
         while(l1 != nullptr){
-            x = (l1->val + up) % 10;
-            up = (l1->val + up) / 10;
-            l1->val = x;
-            pre = l1;
+            x = (up + l1->val) % 10;
+            up = (up + l1->val) / 10;
+            ListNode *node = new ListNode(x);
+            pre->next = node;
+            pre = pre->next;
             l1 = l1->next;
         }
-        // 处理最后进位
-        if(up){
-            ListNode *newNode = new ListNode(up);
-            pre->next = newNode;
+        while(l2 != nullptr){
+            x = (up + l2->val) % 10;
+            up = (up + l2->val) / 10;
+            ListNode *node = new ListNode(x);
+            pre->next = node;
+            pre = pre->next;
+            l2 = l2->next;
         }
-        return head;
+        if(up != 0){
+            ListNode *node = new ListNode(up);
+            pre->next = node;
+        }
+        return dummy.next;
     }
 };
