ac3fdfd1fefeb0da90b45179c57dc8fe95a82749
Aliez22 <820460891@qq.com>
2023-11-06 16:26:51 +0800

    add lc318 最大单词长度乘积

---
+++ 4_String/lc318.最大单词长度乘积.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution318 {
+public:
+    // mask：使用位运算进行优化，mask[i]就是将words[i]转化为位掩码模式
+    int maxProduct(vector<string>& words) {
+        int length = words.size();
+        vector<int> masks(length);
+        for (int i = 0; i < length; i++) {
+            string word = words[i];
+            int wordLength = word.size();
+            for (int j = 0; j < wordLength; j++) {
+                masks[i] |= 1 << (word[j] - 'a');
+            }
+        }
+        int maxProd = 0;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if ((masks[i] & masks[j]) == 0) {
+                    maxProd = max(maxProd, int(words[i].size() * words[j].size()));
+                }
+            }
+        }
+        return maxProd;
+    }
+};
